dc8ef6b8f5b31d98d322fff6b0e3f2a929d4611c
Niveditha Kani <nivedithakani@gmail.com>
2021-08-21 01:24:06 -0400

    Add 3rd endpoint

--- likesanddislikes/likes_and_dislikes/urls.py
+++ likesanddislikes/likes_and_dislikes/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('create-lobby-and-host/', views.create_lobby_and_host, name='create_lobby_and_host'),
     path('join-lobby/', views.join_lobby, name='join_lobby'),
+    path('set_likes_dislikes/', views.set_likes_dislikes, name='set_likes_dislikes'),
+    path('enter_guess/', views.enter_guess, name='enter_guess'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- likesanddislikes/likes_and_dislikes/views.py
+++ likesanddislikes/likes_and_dislikes/views.py
@@ -10,22 +10,37 @@ from likes_and_dislikes.utils import generate_lobby_id, is_valid_lobby
 def create_lobby_and_host(request):
     lobby_id = generate_lobby_id()
     lobby = Lobby.objects.create(lobby_id=lobby_id)
-    
     player_data = {**request.data, "lobby": lobby.pk}
     player_serializer = PlayerSerializer(data=player_data)
     if player_serializer.is_valid():
-        player_serializer.save()
-    return Response(data={"lobby_code": lobby_id}, status=status.HTTP_201_CREATED)
+        player = player_serializer.create(player_serializer.validated_data)
+    return Response(data={"lobby_code": lobby_id, "player_id": player.id, "players_in_lobby": []}, status=status.HTTP_201_CREATED)
 
 @api_view(['POST'])
 def join_lobby(request):
     if not is_valid_lobby(request.data["lobby_id"]):
-        return 
+        return Response(status=status.HTTP_404_NOT_FOUND)
     lobby = Lobby.objects.get(lobby_id=request.data["lobby_id"])
     player_data = {**request.data, "lobby": lobby.pk}
     player_serializer = PlayerSerializer(data=player_data)
     if player_serializer.is_valid():
-        player_serializer.save()
-    print(lobby.list_all_players())
-    print(type(lobby.list_all_players()))
-    return Response(data={"players_in_lobby": lobby.list_all_players()}, status=status.HTTP_201_CREATED)
+        player = player_serializer.create(player_serializer.validated_data)
+    return Response(data={"player_id": player.id, "players_in_lobby": lobby.list_all_players()}, status=status.HTTP_201_CREATED)
+
+@api_view(['POST'])
+def set_likes_dislikes(request):
+    card_data = {**request.data, "player": request.data["player_id"]}
+    card_serializer = CardSerializer(data=card_data)
+    if card_serializer.is_valid():
+        card_serializer.save()
+    print(Card.objects.all())
+    return Response(data={"IT WORKED"}, status=status.HTTP_201_CREATED)
+
+@api_view(['POST'])
+def enter_guess(request):
+    card_data = {**request.data, "player": request.data["player_id"]}
+    card_serializer = CardSerializer(data=card_data)
+    if card_serializer.is_valid():
+        card_serializer.save()
+    print(Card.objects.all())
+    return Response(data={"IT WORKED"}, status=status.HTTP_201_CREATED)
